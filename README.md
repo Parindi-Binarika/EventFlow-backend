e1447ea196483099d01c4ce811a36b02693ae01d
Ashen Kavinda <156800673+AshenKavinda@users.noreply.github.com>
2024-12-20 22:01:28 +0530

    reset project

--- README.md
+++
@@ -1,4 +0,0 @@
-# EventFlow Backend
-
-## Overview
-The backend of the EventFlow application is built using Spring Boot and serves as the core engine for processing business logic, managing data, and providing a secure API for the frontend. This modular architecture allows for scalability and easy maintenance.
--- src/test/java/com/example/backend/BackendApplicationTests.java
+++ src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.eventflowbackend;
+package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EventFlowBackendApplicationTests {
+class BackendApplicationTests {
 
     @Test
     void contextLoads() {
