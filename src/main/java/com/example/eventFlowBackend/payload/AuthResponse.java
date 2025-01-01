12ce6b1d2a6fdb94eab721b300a1c18cd71aeeab
Ashen Kavinda <156800673+AshenKavinda@users.noreply.github.com>
2025-01-01 13:02:25 +0530

    add improvment

--- src/main/java/com/example/eventFlowBackend/payload/AuthResponse.java
+++ src/main/java/com/example/eventFlowBackend/payload/AuthResponse.java
@@ -3,13 +3,19 @@ package com.example.eventFlowBackend.payload;
 // AuthResponse
 public class AuthResponse {
     private String token;
+    private Integer uID;
 
-    public AuthResponse(String token) {
+    public AuthResponse(String token,Integer uID) {
         this.token = token;
+        this.uID = uID;
     }
 
     // Getter
     public String getToken() {
         return token;
+    }
+
+    public Integer getUID() {
+        return uID;
     }
 }
